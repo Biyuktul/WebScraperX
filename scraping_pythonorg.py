c214eb1bbf3a185f1df5206920c4d3bacdd6e961
Yonatan <yonatanaddis0257@gmail.com>
2024-01-06 03:39:23 -0800

    handle url error

--- scraping_pythonorg.py
+++ scraping_pythonorg.py
@@ -1,30 +1,66 @@
 #!/usr/bin/python3
-
+"""
+scraping python.org website to get recent python events\
+    and save the events data to csv
+"""
 from urllib.request import urlopen
+from urllib.error import URLError
 from bs4 import BeautifulSoup
 import csv
+import time
 
-url = "https://www.python.org/events/python-events/"
-page = urlopen(url)
 
-soup = BeautifulSoup(page.read().decode('utf-8'), 'html')
+def download_webpage(url):
+    """
+    Download the content from the specified URL.
 
-events = soup.find('ul', {'class': 'list-recent-events'}).find_all('li')
+    Args:
+        url (str): The URL of the resource to be downloaded.
 
-events_list = []
-for event in events:
-    events_dict = dict()
-    events_dict['Name'] = event.find('h3').find('a').get_text()
-    events_dict['Location'] = event.find('span', {'class': 'event-location'}).get_text()
-    events_dict['Time'] = event.find('time').get_text()
-    events_list.append(events_dict)
+    Returns:
+        string: The string content of the downloaded resource.
 
-with open('events.csv', 'w', encoding='utf-8', newline='') as fh:
-    field_names = ['Name', 'Location', 'Time']
-    writer = csv.DictWriter(fh, fieldnames=field_names)
-    
-    writer.writeheader()
-    for event in events_list:
-        writer.writerow(event)
 
-# [print(event) for event in events_list]
+    Example:
+        >>> content = download('https://www.example.com')
+        >>> print(content)
+    """
+    try:
+        response = urlopen(url).read().decode("utf-8")
+        return response
+    except Exception as e:
+        print("Unexpected error occured\nRetrying...")
+        for i in range(3):
+            try:
+                response = urlopen(url).read().decode("utf-8")
+                return response
+            except Exception as e:
+                print("Failed")
+                time.sleep(5)
+
+
+url = "https://www.python.org/events/python-evnts/"
+html = download_webpage(url)
+
+if html:
+    soup = BeautifulSoup(html, "html")
+
+    events = soup.find("ul", {"class": "list-recent-events"}).find_all("li")
+
+    events_list = []
+    for event in events:
+        events_dict = dict()
+        events_dict["Name"] = event.find("h3").find("a").get_text()
+        events_dict["Location"] = event.find(
+            "span", {"class": "event-location"}
+        ).get_text()
+        events_dict["Time"] = event.find("time").get_text()
+        events_list.append(events_dict)
+
+    with open("events.csv", "w", encoding="utf-8", newline="") as fh:
+        field_names = ["Name", "Location", "Time"]
+        writer = csv.DictWriter(fh, fieldnames=field_names)
+
+        writer.writeheader()
+        for event in events_list:
+            writer.writerow(event)
