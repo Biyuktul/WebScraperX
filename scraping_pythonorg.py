38da48d949fd501b301410d1003e2cc3d4b5dacb
Yonatan <yonatanaddis0257@gmail.com>
2024-01-06 00:08:29 -0800

    write the scraped data to csv

--- scraping_pythonorg.py
+++ scraping_pythonorg.py
@@ -2,6 +2,7 @@
 
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
+import csv
 
 url = "https://www.python.org/events/python-events/"
 page = urlopen(url)
@@ -13,9 +14,17 @@ events = soup.find('ul', {'class': 'list-recent-events'}).find_all('li')
 events_list = []
 for event in events:
     events_dict = dict()
-    events_dict['name'] = event.find('h3').find('a').get_text()
-    events_dict['location'] = event.find('span', {'class': 'event-location'}).get_text()
-    events_dict['time'] = event.find('time').get_text()
+    events_dict['Name'] = event.find('h3').find('a').get_text()
+    events_dict['Location'] = event.find('span', {'class': 'event-location'}).get_text()
+    events_dict['Time'] = event.find('time').get_text()
     events_list.append(events_dict)
 
-[print(event) for event in events_list]
+with open('events.csv', 'w', encoding='utf-8', newline='') as fh:
+    field_names = ['Name', 'Location', 'Time']
+    writer = csv.DictWriter(fh, fieldnames=field_names)
+    
+    writer.writeheader()
+    for event in events_list:
+        writer.writerow(event)
+
+# [print(event) for event in events_list]
